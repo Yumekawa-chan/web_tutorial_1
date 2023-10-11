10c3c8973458f7c402ad1b688ed8e0cf196c424b
Yumekawa_chan <20aj079@ms.dendai.ac.jp>
2023-10-11 23:42:46 +0900

    add javascript file

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello Javascript!";
+
+let ramen = "jiro";
+
+if(ramen == "jiro"){
+    alert("Welcome to my site.")
+}else{
+    alert("good bye.")
+}
+
+function add(num1,num2){
+    let result = num1 + num2;
+    return result;
+}
+
+console.log(add(1,2));
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Don't touch me!!!");
+//   });
+
+document.querySelector("img").addEventListener("click", () => {
+    alert("Don't touch me!!!");
+}); 
+  
+
+let myButton = document.querySelector("button");
+let myHeading2 = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("input yout name");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `javascript is cool!${myName},is cool too!`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Javascript is cool,${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
+  
+  
